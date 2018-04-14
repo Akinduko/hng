152c87bb7052bc53b4ffbeb52cd0d804097ab096
Akinduko Akinwumi <akindukooa@Akindukos-MacBook-Air.local>
2018-04-14 23:13:51 +0100

    hng-v-1.18

--- config.php
+++ config.php
@@ -3,5 +3,7 @@ define ('DB_USER', "porte");
 define ('DB_PASSWORD', "3x0du5P@55w0rd");
 define ('DB_DATABASE', "porte");
 define ('DB_HOST', "10.11.0.218");
+define ('DB_PORT', "3307");
+
 
 ?>
--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 require_once 'config.php';
 
 try {
-    $conn = new PDO("mysql:host=". DB_HOST. ";port=3307;dbname=". DB_DATABASE. , DB_USER, DB_PASSWORD);
+    $conn = new PDO("mysql:host=". DB_HOST. ";port=" . DB_PORT. "; dbname=". DB_DATABASE. , DB_USER, DB_PASSWORD);
     // echo "Connected to ". DB_DATABASE . " successfully.</br>";
 } catch (PDOException $pe) {
     die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
