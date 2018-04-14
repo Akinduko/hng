5cbd50d849dc9f3aed5f8247bf2745aa26d23a7f
Akinduko Akinwumi <akindukooa@Akindukos-MacBook-Air.local>
2018-04-14 23:11:09 +0100

    hng-v-1.17

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 require_once 'config.php';
 
 try {
-    $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE. ";port="5432", DB_USER, DB_PASSWORD);
+    $conn = new PDO("mysql:host=". DB_HOST. ";port=3307;dbname=". DB_DATABASE. , DB_USER, DB_PASSWORD);
     // echo "Connected to ". DB_DATABASE . " successfully.</br>";
 } catch (PDOException $pe) {
     die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
