6e6fef0572fdb2cdef5b282d4fc619410c277a96
Akinduko Akinwumi <akindukooa@Akindukos-MacBook-Air.local>
2018-04-14 23:06:50 +0100

    hng-v-1.15

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 define ('DB_USER', "porte");
 define ('DB_PASSWORD', "3x0du5P@55w0rd");
 define ('DB_DATABASE', "porte");
-define ('DB_HOST', "10.11.0.218:3307");
+define ('DB_HOST', "10.11.0.218");
 
 ?>
