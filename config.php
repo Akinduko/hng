0ad0a59a8d5985b2391686e63a84626b5efed308
Akinduko Akinwumi <akindukooa@Akindukos-MacBook-Air.local>
2018-04-19 17:54:33 +0100

    Auto-deployment script

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define ('DB_USER', "root");
-define ('DB_PASSWORD', "4k1nduk1");
+define ('DB_PASSWORD', "4k1nduk0");
 define ('DB_DATABASE', "hng");
 define ('DB_HOST', "127.0.0.1");
 define ('DB_PORT', "3306");
