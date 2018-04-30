01d33190de6912aff4a79192fc00eae7e38bc673
Akinduko Olugbenga <akindukooa@gmail.com>
2018-04-30 13:45:09 +0100

    work

--- Sign/reset.php
+++ Sign/reset.php
@@ -25,5 +25,40 @@ include_once("header.php");
 </div>
 
 <?php
+$to = "somebody@example.com, somebodyelse@example.com";
+$subject = "HTML email";
+
+$message = <?php echo $_POST["name"]; ?>
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <slayers@hng.fun>' . "\r\n";
+$headers .= 'Cc: slayers@hng.fun' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+?>
+<?php
 include_once("footer.php");
 ?>
