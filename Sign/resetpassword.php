6bf617209b98e78acafdc3474cfb1e3772a0e1d3
Akinduko Olugbenga <akindukooa@gmail.com>
2018-04-30 14:09:26 +0100

    work

--- Sign/resetpassword.php
+++ Sign/resetpassword.php
@@ -11,19 +11,40 @@ include_once("header.php");
                 Enter your email address and we'll send you an email with instructions to reset your password.
             </p>
             <div style="padding: 20px 200px 0px 200px;  width: 800px;">
-                <form action="reset.php" method="post">
-                <input type="text" name="name" class="form-control form-control-lg rounded-right" placeholder="johndoe@example.com" aria-label="Username" aria-describedby="basic-addon1">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                <input type="text" name="email" class="form-control form-control-lg rounded-right" placeholder="johndoe@example.com" aria-label="Username" aria-describedby="basic-addon1">
                 <br />
-                <button class="btn btn-primary btn-block" type="submit" style="border-radius: 8px;">Reset Password</button>
+                <input class="btn btn-primary btn-block" type="submit" style="border-radius: 8px;">Reset Password</input>
+                </form>
                 <p style='color: #ADADAD '>
                      Already have account? <a href="login.php" style="text-decoration: none; "><span style="color: #1E99E0">Log In</span></a> 
-                </form>
                 </p>
             </div>
         </form>
     </div> 
 </div>
 
+<?php
+// define variables and set to empty values
+ $email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $email = test_input($_POST["email"]);
+
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
 <?php
 include_once("footer.php");
 ?>
